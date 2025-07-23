fef81420d7ee69d8cfb32d52e40f0ca0695f148c
Aswin <suryanaryanpattanayak@gmail.com>
2025-07-23 16:27:37 +0530

    final deployment attempt

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
 import "./index.css";
 import App from "./App.jsx";
-import Employee from "./Employee.jsx";
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Employee />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
